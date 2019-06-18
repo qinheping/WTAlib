4c56db3b990fe7f395e4a6b9d1d55256ce019689
qhu28 <qhu28@wisc.edu>
2019-06-18 04:39:37 -0500

    projection

--- src/main/java/utilities/IteFixedPointSolver.java
+++ src/main/java/utilities/IteFixedPointSolver.java
@@ -71,6 +71,67 @@ public class IteFixedPointSolver {
         return result;
     }
 
+
+    private static Set<LinearSet> projection_sls_vs (Set<LinearSet> sl_T,Set<LinearSet> sl_F, Set<Vector<Boolean>> bvSet){
+        Set<LinearSet> result_T = projection_sl_vs(sl_T,bvSet);
+        Set<LinearSet> result_F = projection_sl_vs(sl_F,flip(bvSet));
+        return  SemilinearFactory.dot(result_T,result_F);
+    }
+
+    private static Set<Vector<Boolean>> flip(Set<Vector<Boolean>> bvSet) {
+        Set<Vector<Boolean>> result = new HashSet<>();
+        int dim;
+        for(Vector<Boolean> bv: bvSet){
+            dim = bv.size();
+            Vector<Boolean> newBv = new Vector<>();
+            for(int i = 0; i < dim; i++){
+                newBv.add(!bv.get(i));
+            }
+            result.add(newBv);
+        }
+        return result;
+    }
+
+    private static Set<LinearSet> projection_sl_vs(Set<LinearSet> sl_f, Set<Vector<Boolean>> bvSet) {
+        Set<LinearSet> result = new HashSet<>();
+        for(Vector bv: bvSet){
+            result = SemilinearFactory.union(result,projection_sl_vector(sl_f,bv));
+        }
+        return result;
+    }
+
+    private static Set<LinearSet> projection_sl_vector (Set<LinearSet> sl, Vector<Boolean> bv){
+        Set<LinearSet> result = new HashSet<>();
+        for(LinearSet ls:sl){
+            result.add(projection_ls_vector(ls,bv));
+        }
+        return  result;
+    }
+
+    private static LinearSet projection_ls_vector(LinearSet ls, Vector<Boolean> bv) {
+        Vector<Integer> base = new Vector<>();
+        Set<Vector<Integer>> peroid = new HashSet<>();
+        int dim = bv.size();
+        for(int i = 0; i < dim; i++){
+            if(bv.get(i))
+                base.add(ls.getBase().get(i));
+            else
+                base.add(0);
+        }
+        for(Vector<Integer> pv:ls.getPeriod()){
+            Vector<Integer> newPv = new Vector<>();
+            for(int i = 0; i < dim; i++){
+                if(bv.get(i))
+                    newPv.add(pv.get(i));
+                else
+                    newPv.add(0);
+            }
+        }
+        LinearSet result = new LinearSet(base,peroid);
+        return  result;
+
+    }
+
     private static int getIteCountInExp(Expression exp) {
         switch (exp.type) {
             case 0:
