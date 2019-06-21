a63c30b019871aba45384903a77d0438b0f1313b
Qinheping Hu <qhu28@wisc.edu>
2019-06-21 18:24:45 -0500

    dag

--- src/main/java/utilities/DAG.java
+++ src/main/java/utilities/DAG.java
@@ -1,4 +1,13 @@
 package utilities;
 
+import java.util.List;
+import java.util.Set;
+
 public class DAG {
+    Set<DAGNode> roots;
+    public static class DAGNode{
+        List<DAGNode> successors;
+        Set<Set<String>> reched_nodes;
+        Set<String> value;
+    }
 }
