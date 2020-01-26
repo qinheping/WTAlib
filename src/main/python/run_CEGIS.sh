365bc5b4ff2f4fe4b1c0a50afb55190c066831af
Qinheping Hu <qhu28@wisc.edu>
2020-01-26 02:38:55 -0600

    Update run_CEGIS.sh

--- src/main/python/run_CEGIS.sh
+++ src/main/python/run_CEGIS.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 FILES=../../../benchmarks/CLIA_Track_IF/*
-out_path="../../../benchmarks/result/"
+
 for f in $FILES
 do
-	echo "Processing $f file..." > $out_path$f
+	echo "Processing $f file..." > $out_path".result"
 done
